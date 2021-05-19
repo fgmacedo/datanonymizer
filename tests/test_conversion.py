ccea1c3a1223fa7de74b98ee0d63309bf14de896
Fernando Macedo <fgmacedo@gmail.com>
2021-05-18 23:19:27 -0300

    tests: Fix test

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -22,7 +22,7 @@ def test_coords_to_h3(coords_to_h3):
 
 def test_has_value(has_value):
     assert has_value("") is False
-    assert has_value("   ") is False
+    assert has_value("  ") is True
     assert has_value("0") is True
     assert has_value(".") is True
     assert has_value("Frodo") is True
